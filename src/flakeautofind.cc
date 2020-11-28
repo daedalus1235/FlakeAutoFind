284ab7653486c5acd4627bc9154b612de764dcd4
Charles <cfyang@cmu.edu>
2020-11-28 00:01:43 -0500

    implemented general polynomial regression

--- src/flakeautofind.cc
+++ src/flakeautofind.cc
@@ -22,32 +22,24 @@ using std::vector;
 using namespace cv;
 
 void flatten(Mat, Mat*, float*, float, int);
-Mat bumpContrast(Mat);
-float* findDims(Mat);
 float findMedian(vector<int>);
 vector<vector<int>> ellipsePoints(float*, float*);
 
 int main(int argc, char** argv){
-	cout<<"OpenCV version: "<< CV_VERSION << nl;
+	cout<<"OpenCV version: "<< CV_VERSION << endl;
 
 	string image_path = argv[1];
 
 	Mat img = imread( image_path, IMREAD_COLOR );
 	float dim[] = {img.cols, img.rows};
-	cout<<dim[0] << "x" << dim[1] <<nl;
+	cout<<dim[0] << "x" << dim[1] <<endl;
 
 	if( img.empty()){
 		cout<< "Could not read the image: " << image_path << endl;
 		return -1;
 	}
-	cout<<"Image Loaded!"<<nl;
+	cout<<"Image Loaded!"<<endl;
 	
-
-	Mat contrast = bumpContrast(img);
-
-	Mat blur;
-	//GaussianBlur(img, blur, Size(5,5), 1);
-
 	Mat bgr[3];	
 	split(img, bgr);
 
@@ -60,14 +52,14 @@ int main(int argc, char** argv){
 	float c[2]= {dim[0]/2, dim[1]/2};
 	float ar = dim[0]/dim[1];
 	for( int i = 0; i<3; i++){
-		flatten(bgr[i], flat[i], c, ar, 4);
+		flatten(bgr[i], flat[i], c, ar, 3);
 	}
 
 	float scale = 0.6;
 
 	namedWindow("Debug View", WINDOW_NORMAL);
 	resizeWindow("Debug View", scale*dim[1], scale*dim[0]);
-	cout<<"Opened Debug Window!"<<nl;
+	cout<<"Opened Debug Window!"<<endl;
 	
 	Mat open;
 	Mat const open_kernel = getStructuringElement(MORPH_RECT, Size(7,7));
@@ -82,12 +74,7 @@ int main(int argc, char** argv){
 	morphologyEx(erode,close, MORPH_CLOSE, close_kernel);
 
 	Mat debug(Size(dim[0]*2, dim[1]*2), CV_8UC1);
-	/*
-	bgr[0].copyTo(debug(Rect(0,0,dim[0],dim[1])));
-	bgr[1].copyTo(debug(Rect(dim[0],0,dim[0],dim[1])));
-	bgr[2].copyTo(debug(Rect(0,dim[1],dim[0],dim[1])));
-	flat[2].copyTo(debug(Rect(dim[0],dim[1],dim[0], dim[1])));
-	*/
+
 	bgr[2].copyTo(debug(Rect(0,0,dim[0],dim[1])));
 	cout<<"Displayed Red!"<<nl;
 	flat[2][0].copyTo(debug(Rect(dim[0],0,dim[0],dim[1])));
@@ -95,7 +82,7 @@ int main(int argc, char** argv){
 	flat[2][1].copyTo(debug(Rect(0,dim[1],dim[0],dim[1])));
 	cout<<"Displayed Flattened!"<<nl;
 	close.copyTo(debug(Rect(dim[0],dim[1],dim[0],dim[1])));
-	cout<<"Displayed Morphology Transformed!"<<nl;
+	cout<<"Displayed Morphology Transformed!"<<endl;
 	
 	imshow("Debug View", debug);
 	cout<<"Displayed Debug!"<<endl;
@@ -104,154 +91,112 @@ int main(int argc, char** argv){
 	return 0;
 }
 
-float* findDims(Mat img){
-	return 0;
-}
-
 void flatten(Mat img, Mat* flat, float* c, float ar, int n){
-	cout<<"Samples to be taken: "<<n<<nl;
+	cout<<"Samples to be taken: "<<n<<endl;
 	float dim[2]={img.cols, img.rows};
 	float a[n]; //coefficient array
 	float samp2[n]; //sample locations squared
-	float b[n]; //sample values
+	float b[1][n]; //sample values
 
 	flat[0] = Mat::zeros(img.rows, img.cols, CV_8UC1);//
 	flat[1] = Mat::zeros(img.rows, img.cols, CV_8UC1);//
 	
-	samp2[0]=pow(dim[0],2)/n;
+	samp2[0]=(dim[0]-c[0])*(dim[0]-c[0])+ar*ar*(dim[1]-c[1])*(dim[1]-c[1]);
+	samp2[0]-=2;
+	samp2[0]/=n;
+	cout<<"Sample Separation: "<<samp2[0]<<endl;
+
 	for( int i = 1; i<n; i++){
 		samp2[i] = samp2[i-1]+samp2[0];
 	}//populate s^2 values
 	
 	for( int i = 0; i < n; i++ ){
-		cout<<"entered loop: "<<i<<nl;
-		float r[2] = {sqrt(samp2[i]), ar*sqrt(samp2[i])};
+		float r[2] = {sqrt(samp2[i]), sqrt(samp2[i])/ar};
 	       	vector<vector<int>> ellpts = ellipsePoints(c, r);
-		cout<<"  generated "<<ellpts.size()<<" points"<<nl;
 
 		vector<int> ellvals;
 		for( int j = 0; j<ellpts.size(); j++){
-			int val = img.at<uchar>(ellpts[j][1],ellpts[j][0]);
-			cout<<"  measured point "<<j<<nl;
-			ellvals.push_back(val);
-			cout<<"  recorded measurement "<<j<<nl;
+			int y = ellpts[j][1];
+			int x = ellpts[j][0];
+			if ( x>=0 && y>=0 && x<dim[0] && y<dim[1] ){
+				int val = img.at<uchar>(ellpts[j][1],ellpts[j][0]);
+				ellvals.push_back(val);
+			}
 		}
-		cout<<"took measurements"<<nl;
-		b[i]=findMedian(ellvals);
+		b[0][i]=findMedian(ellvals);
+		cout<<"B"<<i<<": "<<b[0][i]<<endl;
 	}//determine b at each s^2
-	cout<<"Samples Taken!"<<nl;
+	cout<<"Samples Taken!"<<endl;
 
 	//regression
-	int Sarr[n][n];
-	for( int i = 0; i<n; i++){
-		for( int j=0; j<n; i++){
-			if( i == 0)
+	float Sarr[n][n];
+	for( int i = 0; i < n; i++){
+		for( int j=0; j < n; j++){
+			if( j == 0 )
 				Sarr[i][j]=1;
 			else
-				Sarr[i][j]=j*Sarr[i-1][j];
+				Sarr[i][j]=(i+1)*Sarr[i][j-1];
 		}
 	}
 	Mat S(n,n, CV_32F, Sarr);
-	cout<<"S="<<nl<<S<<nl;
+	cout<<"S="<<nl<<S<<endl;
 	
-	int Carr[n][n];
-	for( int i = 0; i<n; i++ ){
-		for( int j=0; j<n; j++){
-			if(j>i){
+	float Carr[n][n];
+	for( int i = 0; i < n; i++ ){
+		for( int j = 0; j < n; j++){
+			if( j > i ){
 				Carr[i][j]=0;
 			}
-			else if (i == 0){
-				if( j==0 )
+			else if ( j == 0 ){
+				if( i == 0 )
 					Carr[i][j]=1;
 				else
-					Carr[i][j]=-Carr[i-1][j];
+					Carr[i][j]=-1*Carr[i-1][j];
 			}
 			else{
-				Carr[i][j]=Carr[i][j-1]*(i-j)/(j+1);
+				Carr[i][j]=-Carr[i][j-1]*(i-j+1)/(j);
 			}
 		}
 	}
 	Mat C(n,n, CV_32F, Carr);
-	cout<<"C="<<nl<<C<<nl;
+	cout<<"C="<<nl<<C<<endl;
 		
 	Mat T=C*S;
-	cout<<"T="<<nl<<T<<nl;
+	cout<<"T="<<nl<<T<<endl;
+
+	Mat B(n,1,CV_32F,b);
+	cout<<"B="<<nl<<B<<endl;
 	
-	/*
+	Mat CB = C*B;
+	cout<<"CB="<<nl<<CB<<endl;
+
+	for( int i = n-1; i >= 0; i--){
+		a[i]=CB.at<float>(i,0);
+		for( int j = i+1; j < n; j++){
+			a[i]-=(T.at<float>(j,i)*a[j]*pow(samp2[j],j));
+		}
+		a[i]/=(T.at<float>(i,i)*pow(samp2[i],i));
+	}
+	cout<<"Coefficients: [";
+	for( int i = 0; i<n; i++){
+		cout<<a[i]<<" ";
+	}
+	cout<<"]"<<endl;
+
 	for( int x = 0; x<dim[0]; x++){
 		for( int y = 0; y<dim[1]; y++){
 			float s2 = (x-c[0])*(x-c[0]) + ar*ar*(y-c[1])*(y-c[1]);
-			float bg = a[n];
-			for( i = n-1; i>=0; i--){
+			float bg = 0;
+			for( int m = n; m > 0; m--){
 				bg*=s2;
-				bg+=a[i];
+				bg+=a[m-1];
 			}
 			flat[1].at<uchar>(y,x)=bg;
 			flat[0].at<uchar>(y,x)=img.at<uchar>(y,x) - bg;
 		}
 	}
-	*/
-	flat[1]=Mat::zeros(img.rows, img.cols, CV_8UC1);
-	flat[0]=img;
-	/*
-	float a[2];
-	float s_1 = dim[0]/3;
-	float s_2 = dim[0]/9;
-
-	float r_1[2] = {s_1, ar*s_1};
-	vector<vector<int>> es1 = ellipsePoints(c, r_1);
-	
-	float r_2[2] = {s_2, ar*s_2};
-	vector<vector<int>> es2 = ellipsePoints(c, r_2);
-
-	vector<int> vals1, vals2;
-	for( int i = 0; i<es1.size(); i++){
-		int val = img.at<uchar>(es1[i][1],es1[i][0]);
-		vals1.push_back(val);
-	}
-	for( int i = 0; i<es2.size(); i++){
-		int val = img.at<uchar>(es2[i][1],es2[i][0]);
-		vals2.push_back(val);
-	}
-
-	float B_1 = findMedian(vals1);
-	float B_2 = findMedian(vals2);
-	
-	a[1]= (B_2-B_1)/(s_2*s_2-s_1*s_1);
-	a[0]= B_1-a[1]*s_1*s_1;
-	
-	cout<<"Coefficients found: a0="<<a[0]<<", a1="<<a[1]<<nl;
-
-	for( int x = 0; x<dim[0]; x++){
-		for( int y = 0; y<dim[1]; y++){
-			float s2 = (x-c[0])*(x-c[0]) + ar * ar * (y-c[1]) * (y-c[1]);
-			float bg = a[0]+a[1]*s2;
-			flat[1].at<uchar>(y,x)=bg;
-			flat[0].at<uchar>(y,x)=img.at<uchar>(y,x) -bg;
-		}
-	}
-	*/
 }
 
-Mat bumpContrast(Mat img){
-	int dim[2]={img.cols, img.rows};
-	float scale = 0.4;
-	Mat grey;
-        cvtColor(img, grey, COLOR_BGR2GRAY);
-	
-	int avg = mean(grey)[0]+5;	
-
-	Mat contrast = Mat::zeros(img.size(), img.type() );
-	float a = 100; 
-	for( int y = 0; y< dim[1]; y++){
-		for( int x = 0; x < dim[0]; x++){
-			for( int c = 0; c < img.channels(); c++){
-				contrast.at<Vec3b>(y,x)[c] = saturate_cast<uchar>(a * (img.at<Vec3b>(y,x)[c]-avg));
-			}
-		}
-	}
-	return contrast;
-}
 
 float findMedian(vector<int> values){
 	vector<int> sorted = values;
