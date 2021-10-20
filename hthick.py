89385c14ed1d178a324e3f4e5235dc86792b1e1c
Charles <cfyang@cmu.edu>
2021-10-20 12:32:16 -0400

    Ported Flatten

--- hthick.py
+++ hthick.py
@@ -9,20 +9,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--input', default = 'images/bg100.jpg', help='Name of Input File (default: images/bg100.jpeg)')
     args=parser.parse_args()
-    
-    ellipse = ellipsePoints([50,150],[25,100])
-    test = np.zeros((101,301),np.uint8)
-    print(ellipse)
-    for point in ellipse:
-        print(point)
-        test.itemset(point, 255)
-    
 
     img = cv.imread(args.input)
     if img is None:
         sys.exit("Could not read the image.")
     
-    cv.imshow("Display Window", test)
+    dim = img.shape[0:2]
+
+    n = 3
+
+    flat=[None, None]
+
+    b,g,r = cv.split(img)
+
+    ar = dim[0]/dim[1]
+    a = [0]*n
+    c = (1400,900)
+
+    flatten(r, flat, a, c, ar, n)
+
+    cv.imshow("Display Window", flat[0])
     k = cv.waitKey(0)
 
 
--- src_hthick/faf_fns.py
+++ src_hthick/faf_fns.py
@@ -1,5 +1,78 @@
-import cv2 as cv
+import math
+import statistics
 import numpy as np
+import cv2 as cv
+
+def flatten(img, flat, a, c, ar, n):
+    dim = img.shape[0:2]
+    samp2 = [0]*n
+    Barr=[[0]]*n
+
+    flat[0] = np.zeros(dim, np.uint8)
+    flat[1] = np.zeros(dim, np.uint8)
+
+    ra = max(c[0], abs(dim[0]-c[0]))
+    rb = max(c[1], abs(dim[1]-c[1]))
+    samp2[0] = ra*ra + ar*ar*rb*rb
+    samp2[0]*= 0.99
+    samp2[0]/= n
+
+    for i in range(1,n):
+        samp2[i] = samp2[i-1]+samp2[0]
+    
+    for i in range(n):
+        r = [math.sqrt(samp2[i]), math.sqrt(samp2[i])]
+        ellpts = ellipsePoints(c,r)
+
+        ellvals = []
+        for point in ellpts:
+            if point[0]>=0 and point[1]>=0 and point[0]<dim[0] and point[1]<dim[1]:
+                ellvals.append(img.item(point))
+
+        Barr[i][0] = statistics.median(ellvals)
+    
+    Sarr=[[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if j==0:
+                Sarr[i][j]=1
+            else:
+                Sarr[i][j] = (1+i)*Sarr[i][j-1]
+    S = np.array(Sarr)
+
+    Carr=[[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if j>i:
+                Carr[i][j] = 0
+            elif j==0:
+                if i==0:
+                    Carr[i][j] =1
+                else:
+                    Carr[i][j] =-Carr[i-1][j]
+            else:
+                Carr[i][j] =-Carr[i][j-1]*(i-j+1)/j
+    C = np.array(Carr)
+    B = np.array(Barr)
+    T = np.matmul(C,S)
+    CB = np.matmul(C,B)
+
+    for i in range(n-1, -1, -1):
+        a[i] = CB.item((i,0))
+        for j in range(i+1, n):
+            a[i]-=(T.item((i,j))*a[j]*pow(samp2[j],j))
+        a[i]/=(T.item(i,i)*pow(samp2[i],i))
+
+    for x in range(dim[0]):
+        for y in range(dim[1]):
+            s2 = (x-c[0])**2 +  (ar*(y-c[1]))**2
+            bg = 0
+            for m in range(n,0,-1):
+                bg*=s2
+                bg+-a[m-1]
+            flat[1].itemset((x,y),bg)
+            flat[0].itemset((x,y), img.item((x,y))-bg)
+    
 
 def ellipsePoints(c, r):
     ellipse = []
@@ -11,11 +84,6 @@ def ellipsePoints(c, r):
     dx = 2*r[1]*r[1]*x
     dy = 2*r[0]*r[0]*y
     
-    ellipse.append(( x+c[0], y+c[1]))
-    ellipse.append((-x+c[0], y+c[1]))
-    ellipse.append(( x+c[0],-y+c[1]))
-    ellipse.append((-x+c[0],-y+c[1]))
-
     while dx<dy:
         v1 = (int( x+c[0]),int( y+c[1]))
         v2 = (int(-x+c[0]),int( y+c[1]))
@@ -40,7 +108,7 @@ def ellipsePoints(c, r):
     d2 = ( (r[1]*r[1])*((x+0.5)*(x+0.5)) )\
             +( (r[0]*r[0]) * ((y-1)*(y-1)) )\
             -( r[0]*r[0]*r[1]*r[1])
-    while y>0:
+    while y>=0:
         v1 = (int( x+c[0]), int( y+c[1]))
         v2 = (int(-x+c[0]), int( y+c[1]))
         v3 = (int( x+c[0]), int(-y+c[1]))
